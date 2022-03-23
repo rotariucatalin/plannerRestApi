2a28709fc6d04212667fff7c7a998a45ba440dc3
rotariucatalin <rotariucatalin90@gmail.com>
2022-03-23 09:54:49 +0200

    Adding minimum for security

---
+++ src/main/java/com/example/plannerRestAPI/security/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.plannerRestAPI.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+
+    
+
+}
