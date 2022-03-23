c3519ec8986e0da531a808df590f91eea898dadf
rotariucatalin <rotariucatalin90@gmail.com>
2022-03-23 09:24:52 +0200

    Initialize project

---
+++ src/main/java/com/example/plannerRestAPI/PlannerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.plannerRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plannerRestAPI/PlannerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plannerRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
